1d23a41280d73bbcbdb328ed3461481e2070a025
17021056 <37436327+17021056@users.noreply.github.com>
2018-04-09 15:35:58 +0700

    Update main.cpp

--- Gametictactoe/main.cpp
+++ Gametictactoe/main.cpp
@@ -1,13 +1,12 @@
 #include <iostream>
 #include<windows.h>
 #include"console.h"
+#include"giaodiengame.h"
+#include"ktrachienthang.h"
 using namespace std;
 
-char square[10] = {'0','1','2','3','4','5','6','7','8','9'};
-int checkwin();
-void board();
 int main() {
-
+	char square[10] = {'0','1','2','3','4','5','6','7','8','9'};
 	int player = 1,choice;
     int i,j;
     for(j=1;j<=10;j++) {
@@ -111,52 +110,3 @@ int main() {
 	}
 }
 
-int checkwin() {
-	if (square[1] == square[2] && square[2] == square[3])
-
-		return 1;
-	else if (square[4] == square[5] && square[5] == square[6])
-
-		return 1;
-	else if (square[7] == square[8] && square[8] == square[9])
-
-		return 1;
-	else if (square[1] == square[4] && square[4] == square[7])
-
-		return 1;
-	else if (square[2] == square[5] && square[5] == square[8])
-
-		return 1;
-	else if (square[3] == square[6] && square[6] == square[9])
-
-		return 1;
-	else if (square[1] == square[5] && square[5] == square[9])
-
-		return 1;
-	else if (square[3] == square[5] && square[5] == square[7])
-
-		return 1;
-	else if (square[1] != '1' && square[2] != '2' && square[3] != '3'
-            && square[4] != '4' && square[5] != '5' && square[6] != '6'
-            && square[7] != '7' && square[8] != '8' && square[9] != '9')
-
-		return 0;
-	else
-		return -1;
-}
-
-void board() {
-      system("cls");
-            cout << "\n\ntic Tac Toe\n\n";
-
-	cout << "Player 1 (X)  -  Player 2 (O)" << endl << endl;
-	cout << endl;
-    cout <<char(218)<<char(196)<<char(196)<<char(196)<<char(196)<<char(194)<<char(196)<<char(196)<<char(196)<<char(196)<<char(194)<<char(196)<<char(196)<<char(196)<<char(196)<<char(191)<<endl;
-    cout <<char(179)<<"  "<<square[1]<<" "<<char(179)<<"  "<<square[2]<<" "<<char(179)<<"  "<<square[3]<<" "<<char(179)<<endl;
-    cout <<char(195)<<char(196)<<char(196)<<char(196)<<char(196)<<char(197)<<char(196)<<char(196)<<char(196)<<char(196)<<char(197)<<char(196)<<char(196)<<char(196)<<char(196)<<char(180)<<endl;
-    cout <<char(179)<<"  "<<square[4]<<" "<<char(179)<<"  "<<square[5]<<" "<<char(179)<<"  "<<square[6]<<" "<<char(179)<<endl;
-    cout <<char(195)<<char(196)<<char(196)<<char(196)<<char(196)<<char(197)<<char(196)<<char(196)<<char(196)<<char(196)<<char(197)<<char(196)<<char(196)<<char(196)<<char(196)<<char(180)<<endl;
-    cout <<char(179)<<"  "<<square[7]<<" "<<char(179)<<"  "<<square[8]<<" "<<char(179)<<"  "<<square[9]<<" "<<char(179)<<endl;
-    cout <<char(192)<<char(196)<<char(196)<<char(196)<<char(196)<<char(193)<<char(196)<<char(196)<<char(196)<<char(196)<<char(193)<<char(196)<<char(196)<<char(196)<<char(196)<<char(217)<<endl;
-
-}
